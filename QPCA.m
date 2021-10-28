7dda64855b5fa53d05106dbb8fb15826802d72b3
PeterWDavies <85742965+PeterWDavies@users.noreply.github.com>
2021-10-28 20:53:43 +0100

    Add files via upload

--- QPCA.m
+++ QPCA.m
@@ -13,59 +13,38 @@ function [w, info] = QPCA(problem, w, options)
     if ~isfield(options, 'bits'); options.bits = 4; end % Quantization bits per entry.
     if ~isfield(options, 'batchsize'); options.batchsize = n/100; end % Batchsize.
     if ~isfield(options, 'num_worker'); options.num_worker = 100; end % number of workers
-    if ~isfield(options, 'maxinneriter'); options.maxinneriter = 0; end % Maximum number of sampling per epoch.
-    if ~isfield(options, 'flag_random_shuffle'); options.flag_random_shuffle = 0; end % 
     if ~isfield(options, 'average_strategy'); options.average_strategy = 'average'; end
 
     
-    stepsize = options.stepsize; % for display
-    bits= options.bits;
-
+    stepsize = options.stepsize; 
     problem_local = rmfield(problem,{'cost','efullgrad'});
     
-    max_inneriter =floor(options.maxinneriter);
-    opt_local.maxinneriter = max_inneriter;
     opt_local.batchsize = options.batchsize;
     opt_local.stepsize = stepsize;
     
     num_worker = options.num_worker;
-    if options.flag_random_shuffle == 1
-        idx_data_shuffled = randperm(n); % random shuffling
-    else
-        idx_data_shuffled = 1:n; % no shuffling.
-    end
-
+    idx_data_shuffled = randperm(n); % randomly permute the data for assignment to workers
     
-    % balance data segmentation
+    % assign data
     size_data_block = floor(n/num_worker);
     for i = 1:num_worker
         idx_block{i} = idx_data_shuffled(((i-1)*size_data_block+1):(i*size_data_block));
     end
     
     if isempty(w) % if init w is not given, generate it randomly.
-        x1_sample = cell2mat(x(idx_block{1}));
-        X1 = x1_sample * x1_sample' / size_data_block;
-        [V,D] = eig(X1);
-        w_outer =  V(:,size(X1,2));
-        clear x1_sample X1 V D
+        w_init = normrnd(0,1,[d,1]);
+        w_current = w_init/norm(w_init);
     else
-        w_outer = w;
+        w_current = w;
     end
     
 
     fprintf('-------------------------------------------------------%5d\n',num_worker);
-    fprintf('CEDRE:  epoch\t               cost val\t    grad. norm\t stepsize\n');
-
-    
-% master
-
-    %epstepsize = 1:options.maxepoch;
-    %epstepsize = 1.3.^epstepsize / sqrt(d);
-    %epstepsize = min(epstepsize,ones(1,options.maxepoch)*opt_local.stepsize);
+    fprintf('QPCA:  epoch\t               cost val\t    grad. norm\t stepsize\n');
     
     if options.lrtype == 'const'
         epstepsize = ones(options.maxepoch)*options.stepsize;
-    elseif options.lrtype == 'accel'
+    elseif options.lrtype == 'accel'    %Accelerated learning-rate schedule, as suggested by theory - recommend using const in practice
         B = options.maxepoch/sqrt(d);
         steps = 1:options.maxepoch;
         steps = min(steps, ones(1,options.maxepoch)*(options.maxepoch-B));
@@ -75,39 +54,28 @@ function [w, info] = QPCA(problem, w, options)
         epstepsize=epstepsize*options.stepsize;
     end    
         
-    grad_worker_old = zeros(d,num_worker);
     grad_worker = zeros(d,num_worker);     
-    grad_worker_diff = zeros(d,num_worker); 
     grad_avg_old = zeros(d,1);
     grad_avg = zeros(d,1);
-    grad_avg_diff = zeros(d,1); 
     old = zeros(d,num_worker);
-    rgrad_worker_old = zeros(d-1,num_worker);
     rgrad_worker = zeros(d-1,num_worker);     
-    rgrad_worker_diff = zeros(d-1,num_worker);    
-    
-    
- 
-    rgrad_avg = zeros(d-1,1);
 
-    
+   
     for epoch = 1:options.maxepoch
         
-        egrad = problem.efullgrad(w_outer);
-        rgrad_full = problem.M.egrad2rgrad(w_outer,egrad);
-        
-        gradnorm = problem.M.norm(w_outer, rgrad_full);
+        egrad = problem.efullgrad(w_current);
+        rgrad_full = problem.M.egrad2rgrad(w_current,egrad);
+        gradnorm = problem.M.norm(w_current, rgrad_full);
         info(epoch).gradnorm = gradnorm;
-        cost = problem.cost(w_outer);
+        cost = problem.cost(w_current);
         info(epoch).loss = cost; 
-        fprintf('CEDRE:  %5d\t%+.16e\t%.8e\t%.8e\n',epoch-1, cost, gradnorm, epstepsize(epoch));
+        fprintf('QPCA:  %5d\t%+.16e\t%.8e\t%.8e\n',epoch-1, cost, gradnorm, epstepsize(epoch));
         
-
         %if quantization is in Euclidean space:
         if ~strcmp(options.quant,'rquant')
             % call local worker to perform local computation
             for i = 1:num_worker
-                grad_worker(:,i) = worker_grad(problem_local,w_outer,opt_local,idx_block{i});
+                grad_worker(:,i) = worker_grad(problem_local,w_current,opt_local,idx_block{i});
             end
             grad_worker_diff = grad_worker-old;
         
@@ -117,16 +85,15 @@ function [w, info] = QPCA(problem, w, options)
             
             grad_avg = grad_avg_diff + grad_avg_old;  
 
-            %convert to riemannian & take step (egrad2rgrad takes column
-            %vector as grad
-            new_w_grad = problem.M.egrad2rgrad(w_outer,grad_avg);      
+            %convert to riemannian & take step 
+            new_w_grad = problem.M.egrad2rgrad(w_current,grad_avg);      
             
         else %quantization is in tangent space
-            obasis = tangentorthobasis(problem.M, w_outer);     
+            obasis = tangentorthobasis(problem.M, w_current);     
             for i = 1:num_worker
-                workergrad = worker_grad(problem_local,w_outer,opt_local,idx_block{i});
-                worker_rgrad = problem.M.egrad2rgrad(w_outer,workergrad);
-                vec = tangent2vec(problem.M, w_outer ,obasis, worker_rgrad);
+                workergrad = worker_grad(problem_local,w_current,opt_local,idx_block{i});
+                worker_rgrad = problem.M.egrad2rgrad(w_current,workergrad);
+                vec = tangent2vec(problem.M, w_current ,obasis, worker_rgrad);
                 rgrad_worker(:,i) = vec;
             end
             
@@ -134,40 +101,34 @@ function [w, info] = QPCA(problem, w, options)
             rgrad_avg = quantavgdiff(rgrad_worker,options);  
             
             %convert back to full euclidean
-            
-            new_w_grad = lincomb(problem.M, w_outer,obasis, rgrad_avg);             
-            %tangentgrads = obasis* grad_worker;
+            new_w_grad = lincomb(problem.M, w_current,obasis, rgrad_avg);            
 
         end
         
-        w_outer1 = w_outer;
-        w_outer =  problem.M.exp(w_outer, new_w_grad, -epstepsize(epoch));   
+        w_current =  problem.M.exp(w_current, new_w_grad, -epstepsize(epoch));   
 
-        %rescale to norm 1 to prevent rounding errors
-        w_outer = w_outer/norm(w_outer);
+        %rescale to norm 1 to deal with rounding errors within M.exp()
+        w_current = w_current/norm(w_current);
         
         %update old values
         grad_avg_old = grad_avg;
         grad_worker_old = grad_worker;
 
     end
-    
-    w = w_outer;
+    w = w_current;
     
     
     
     function [w_grad] = worker_grad(problem,w,options,idx_block)
-        % load local data
         x_local = x(idx_block,:);   
         perm_idx = randperm(length(idx_block));
         x_perm = x_local(perm_idx,:);
-        x_batch = x_perm(1 : options.batchsize,:);   
-        w_grad = -2 * x_batch'*x_batch*w;
-        %w_grad = egrad(w);
+        x_batch = x_perm(1 : options.batchsize,:);  %Pick a random sample of size batchsize from the worker's local data
+        w_grad = problem.egrad(w,x_batch);  %return the Euclidean gradient on that sample
     end
 
 
-    function [qad,old] = quantavgdiff(grad_worker_diff,options)
+    function [qad,old] = quantavgdiff(grad_worker_diff,options) %Simulates the quantizing, collection, averaging, requantizing, and broadcasting of vectors
         [dim,numworkers] = size(grad_worker_diff);
         qad = zeros(dim,1);
         old = zeros(dim,options.num_worker);
@@ -175,14 +136,14 @@ function [w, info] = QPCA(problem, w, options)
             old(:,i)=quantvector(grad_worker_diff(:,i),options);
             qad = qad + old(:,i)/numworkers;
         end     
-        qad = quantvector(qad,options);
+        qad = quantvector(qad,options);     %Also returns the vectors' quantized values, since these are needed next round for difference quantization
     end
 
-    function [quant_vec] = quantvector(v,options)
-        if strcmp(options.quant,'none')
+    function [quant_vec] = quantvector(v,options) %Quantize a given vector
+        if strcmp(options.quant,'none') %(If full-precision, just return the vector)
            quant_vec = v;
         else
-            amax = max(abs(v));
+            amax = max(abs(v)); %Else, use unbiased coordinate-wise rounding procedure
             side = 2*amax / (2^(options.bits-1));
             offset = rand;
             roundv = round(v/side+offset);
--- runQPCA.m
+++ runQPCA.m
@@ -9,31 +9,10 @@ D(isnan(B))=0;
 [w,info] = runQPCA1(C);
 
 function [w,info] = runQPCA1(dataset)
-% function [w,info] =runCEDRE(input_filename,opt)
 
-% setting of opt:
-% maxepoch: max global iterations
-% tolgradnorm: tolerance of full gradient norm
-% stepsize: step size
-% batchsize: batch size
-% num_worker: number of workers
-% maxinneriter: max local iterations
-% flag_random_shuffle: Set true to randomly shuffle the data
-% average_strategy: 'random'-random selection; 'average'-sign-fixed average
-%   'geo-avg'-geodesically averaging
-
-    input_filename = 'examgrades.mat';
-    %input_data = examgrades;
-    %input_data.x_sample = normrnd(0, 1, 1000,200);
-    
-    x_sample = dataset;
-
-    %x_sample = eye(1000);
-    %x_sample(1,1) = 2;
-    [n,d] = size(x_sample);
-    disp(size(x_sample));
-    % data
-    data.x = x_sample;
+    [n,d] = size(dataset);
+    disp(size(dataset));
+    data.x = dataset;
     problem.data = data;
 
     % Create the problem structure.
@@ -43,7 +22,7 @@ function [w,info] = runQPCA1(dataset)
     % Define the problem cost function and its Euclidean gradient.
     problem.cost = @cost;
     function f = cost(w)
-        f = - w' * x_sample' * x_sample * w;
+        f = - w' * dataset' * dataset * w;
         f = f/n;
     end
 
@@ -54,31 +33,21 @@ function [w,info] = runQPCA1(dataset)
 
     problem.efullgrad = @efullgrad;
     function g = efullgrad(w)
-        g = - 2* x_sample'*(x_sample*w);
+        g = - 2* dataset'*(dataset*w);
     end
-
-    problem.egrad_batchsize = @egrad_batchsize;
-    function g = egrad_batchsize(U, data_batchsize)
-        x_sample_batchsize = cell2mat(data_batchsize);        
-        N_batchsize = size(x_sample_batchsize, 2);
-        g =  -x_sample_batchsize*(x_sample_batchsize'*U);
-        g = g/N_batchsize;
-    end
-                               %Now just need to average over runs
+                             
     options=[];
     options.maxepoch=31;
-    options.stepsize =1e-9;   %1e-9 for A 1 to 5 e-5 for B, 1e-9 for C, 1e-4 for D 
+    options.stepsize =1e-9;   %informative step sizes: 1e-9 for A 1 to 5 e-5 for B, 1e-9 for C, 1e-4 for D 
     options.num_worker = 5;
-    options.batchsize = int32(n/options.num_worker);
-    options.maxinneriter = 1;
-    options.flag_random_shuffle = 0;
-    options.bits=10;
+    options.batchsize = int32(n/options.num_worker);    %Must be at most floor(n/options.num_worker)
+    options.bits=4;
     options.lrtype = 'const';
     
     w_init = normrnd(0,1,[d,1]);
     w_init = w_init/norm(w_init);
     
-    runs = 10;
+    runs = 1;
     
     sum0=zeros(1,options.maxepoch);
     sum1=zeros(1,options.maxepoch);    
@@ -101,11 +70,8 @@ function [w,info] = runQPCA1(dataset)
         
         options.quant = 'quant';  
         [w3, info3] = QPI(problem, w_init, options);
-        
-        %options.bits=8;
-        %start = info3(1).iterate;
-        %[w3, info3] = QPCA(problem, start, options);
-        sum3=sum3+[info3.loss];
+        sum3=sum3+[info3.loss];        
+
     end
     
     avg0 = sum0 / runs;
